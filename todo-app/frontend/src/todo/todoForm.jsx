c063d243e2ecbc5ace266f9c5601423152dc2442
Erick Henrique dos Santos <erickhenrique321@gmail.com>
2019-04-10 17:12:12 -0300

    controlling input and button

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -6,7 +6,10 @@ export default props => (
   <div role='form' className='todoForm'>
     <Grid cols='12 9 10'>
       <input id="description" className='form-control'
-          placeholder='Adicionar uma tarefa'/>
+          placeholder='Adicionar uma tarefa'
+          value={props.description}
+          onChange={props.handleChange}
+          />
     </Grid>
     <Grid cols='12 3 2'>
       <IconButton style='primary' icon='plus'
