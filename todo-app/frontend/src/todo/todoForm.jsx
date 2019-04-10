d9c50382ba2e7ba7da1d6df8301cee4b546131e9
Erick Henrique dos Santos <erickhenrique321@gmail.com>
2019-04-10 16:11:53 -0300

    click add button; if tag

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -9,7 +9,9 @@ export default props => (
           placeholder='Adicionar uma tarefa'/>
     </Grid>
     <Grid cols='12 3 2'>
-      <IconButton style='primary' icon='plus'></IconButton>
+      <IconButton style='primary' icon='plus'
+        onClick={props.handleAdd}
+      ></IconButton>
     </Grid>
   </div>
 )
