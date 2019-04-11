9520f4ae2bad3b6efe082732a58a2b78f76e7376
Erick Henrique dos Santos <erickhenrique321@gmail.com>
2019-04-11 16:09:56 -0300

    keys

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -2,25 +2,40 @@ import React from 'react'
 import Grid from '../main/grid'
 import IconButton from '../template/iconButton'
 
-export default props => (
-  <div role='form' className='todoForm'>
-    <Grid cols='12 9 10'>
-      <input id="description" className='form-control'
-          placeholder='Adicionar uma tarefa'
-          value={props.description}
-          onChange={props.handleChange}
-          />
-    </Grid>
-    <Grid cols='12 3 2'>
-      <IconButton style='primary' icon='plus'
-        onClick={props.handleAdd} >
-      </IconButton>
-      <IconButton style='info' icon='search'
-        onClick={props.handleSearch}>
-      </IconButton>
-      <IconButton style='default' icon='close'
-        onClick={props.handleClear}>
-      </IconButton>
-    </Grid>
-  </div>
-)
+export default props => {
+  const keyHandler = (e) => {
+    switch (e.key) {
+      case 'Enter':
+        e.shiftKey ? props.handleSearch() : props.handleAdd()
+        break;
+      case 'Escape':
+        props.handleClear()
+      default:
+        break;
+    }
+  }
+
+  return (
+    <div role='form' className='todoForm'>
+      <Grid cols='12 9 10'>
+        <input id="description" className='form-control'
+            placeholder='Adicionar uma tarefa'
+            value={props.description}
+            onChange={props.handleChange}
+            onKeyUp={keyHandler}
+            />
+      </Grid>
+      <Grid cols='12 3 2'>
+        <IconButton style='primary' icon='plus'
+          onClick={props.handleAdd} >
+        </IconButton>
+        <IconButton style='info' icon='search'
+          onClick={props.handleSearch}>
+        </IconButton>
+        <IconButton style='default' icon='close'
+          onClick={props.handleClear}>
+        </IconButton>
+      </Grid>
+    </div>
+  )
+}
