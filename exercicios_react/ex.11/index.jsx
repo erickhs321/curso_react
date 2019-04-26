f6f8fb9ba003f5e5c92947eff497fb3be9c2f4b6
Erick Henrique dos Santos <erickhenrique321@gmail.com>
2019-04-26 15:53:13 -0300

    counter redux

--- exercicios_react/ex.11/index.jsx
+++ exercicios_react/ex.11/index.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { CombineReducers, createStore, combineReducers } from 'redux' //métodos criar store (objeto com vários atributos que é controlado por um reducer)
 import { Provider } from 'react-redux' //Componente para passar estado para componentes internos
-import counterReducer from './counterReducer'
-import Counter from './counter'
+
+import Field from './field'
+import fieldReducer from './fieldReducer'
 
 const reducers = combineReducers({
-  counter: counterReducer
+  field: fieldReducer
 })
 
 ReactDOM.render(
   <Provider store={createStore(reducers)}>
-    <Counter />
+    <Field initialValue='Teste' />
   </Provider>
 , document.getElementById('app'))
